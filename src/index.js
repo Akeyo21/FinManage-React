1b0d788e09bda909101016c4bb06f3f9a3e58395
Akeyo21 <vickyakeke@gmail.com>
2023-05-23 20:00:21 +0300

    Moving app router to its own file

--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import PastBills from "./PastBills.tsx";
 import reportWebVitals from "./reportWebVitals";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/past-bills",
-        element: <PastBills />,
-      },
-    ],
-  },
-]);
+import "./index.css";
+import { appRoutes } from "./Router/Routes.tsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={appRoutes} />
   </React.StrictMode>
 );
 
