a174e8968af785c815d4a8b0ce302bba5418045f
Akeyo21 <vickyakeke@gmail.com>
2023-05-23 19:59:54 +0300

    Move router to its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,18 @@
-import ListItem from "@mui/material/ListItem";
-import List from "@mui/material/List";
-import Drawer from "@mui/material/Drawer";
-import ListItemText from "@mui/material/ListItemText";
 import "./App.css";
-// import Link from "@mui/material/Link";
-import ListItemButton from "@mui/material/ListItemButton";
-import {
-  Link as RouterLink,
-  LinkProps as RouterLinkProps,
-} from "react-router-dom";
 import * as React from "react";
 import { Outlet } from "react-router-dom";
+import Box from "@mui/material/Box";
+import AppRouter from "./Router/App.router.tsx";
 
 function App() {
-  const Link = React.forwardRef<HTMLAnchorElement, RouterLinkProps>(
-    function Link(itemProps, ref) {
-      return <RouterLink ref={ref} {...itemProps} role={undefined} />;
-    }
-  );
 
   return (
     <>
-      <Drawer variant="permanent">
-        <List>
-          {/* <ListItem components={{Link}} to='/'> */}
-          <ListItemButton component={Link} to={"/"}>
-            <ListItemText primary="Home" />
-          </ListItemButton>
-          {/* </ListItem> */}
+      <AppRouter/>
 
-          {/* <ListItem > */}
-          <ListItemButton component={Link} to={"/past-bills"}>
-            <ListItemText primary="Home 2" />
-          </ListItemButton>
-          {/* </ListItem> */}
-        </List>
-      </Drawer>
-
-      <div id="detail">
+      <Box ml={10}>
         <Outlet />
-      </div>
+      </Box>
     </>
   );
 }
