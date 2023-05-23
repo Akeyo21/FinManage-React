5163193671807c9c661b56cd5aa60e5c6ab53d04
Akeyo21 <vickyakeke@gmail.com>
2023-05-23 20:00:09 +0300

    Adding in the bills and due bills pages

---
+++ src/Bills.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from "@mui/material"
+
+const Bills = ()=>{
+  return(
+    <Typography>ALL BILLS</Typography>
+  )
+}
+
+export default Bills
---
+++ src/DueBills.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Typography } from "@mui/material"
+
+const DueBills = ()=>{
+  return(
+    <Typography>DUE BILLS</Typography>
+  )
+}
+export default DueBills
