d06fad94fec25fb4304fc364f3cd41f6600d11e2
Akeyo21 <vickyakeke@gmail.com>
2023-05-24 20:54:17 +0300

    Delete unused imports

--- src/screens/Bills/Bills.tsx
+++ src/screens/Bills/Bills.tsx
@@ -1,8 +1,5 @@
-// import React from 'react'
-import { Grid, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import AllBillsContainer from "./components/AllBillsContainer.tsx";
-import { drawerBackgroundColor } from "../../utils/palette.ts";
-import SectionHeader from "../../components/SectionHeader.tsx";
 import BillsByDateContainer from "./components/BillsByDateContainer.tsx";
 
 const Bills = () => {
