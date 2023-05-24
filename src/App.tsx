4d91be04c5e35f1f74948584c526dbf1be2540db
Akeyo21 <vickyakeke@gmail.com>
2023-05-24 19:13:49 +0300

    Fix overflow issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Box from "@mui/material/Box";
-import "./App.css";
 
 import AppRouter from "./router/App.router.tsx";
+import { mainBackgroundColor } from "./utils/palette.ts";
 
 export const drawerWidth = 250;
 function App() {
-
   return (
-    <div className="App" >
-      <AppRouter/>
-      <Box component="main" sx={{ml:`${drawerWidth}px`, width:`calc(100% - ${drawerWidth}px)`  }}>
+    <div style={{ overflow: "hidden", backgroundColor:mainBackgroundColor, height: '100vh' }}>
+      <AppRouter />
+      <Box
+        component="main"
+        sx={{
+          ml: `${drawerWidth}px`,
+          width: `calc(100% - ${drawerWidth}px)`,
+          overflow: "hidden",
+        }}
+      >
         <Outlet />
       </Box>
     </div>
