10ff4bbd577bf49e6c93e15f165cc37f9c6022d3
Akeyo21 <vickyakeke@gmail.com>
2023-05-24 20:44:51 +0300

    Add util file to get the bills by unique date

---
+++ src/screens/Bills/components/UniqueDueDateBills.ts
@@ -0,0 +1,15 @@
+import { billSamples } from "../Bills.sample.ts";
+
+export const billsByUniqueDueDate = () => {
+  const allDueDates = billSamples.map(({ dueDate }) => dueDate).flat();
+  const uniqueDueDates = Array.from(new Set(allDueDates));
+
+  const billsByUniqueDueDateMap = uniqueDueDates.reduce((acc, cur) => {
+    return {
+      ...acc,
+      [cur]: billSamples.filter(({ dueDate }) => dueDate.includes(cur)),
+    };
+  }, {});
+
+  return { dates: uniqueDueDates, billsDueDateMap: billsByUniqueDueDateMap };
+};
