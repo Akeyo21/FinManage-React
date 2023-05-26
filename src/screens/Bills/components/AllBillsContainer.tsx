f7666dc155f64aec740a42f989d1b0d6b8d507f8
Akeyo21 <vickyakeke@gmail.com>
2023-05-26 19:46:07 +0300

    Format the file

--- src/screens/Bills/components/AllBillsContainer.tsx
+++ src/screens/Bills/components/AllBillsContainer.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
 import BillCard from "./BillCard";
 import { billSamples } from "../Bills.sample";
-import { drawerBackgroundColor } from "../../../utils/palette";
+import {
+  drawerBackgroundColor,
+} from "../../../utils/palette";
 import SectionHeader from "../../../components/SectionHeader";
 
 const AllBillsContainer = () => {
