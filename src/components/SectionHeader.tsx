5b1f797c3be04f8481910717f3f7fcb5d2c69650
Akeyo21 <vickyakeke@gmail.com>
2023-05-26 19:46:00 +0300

    Add the add button within the section header

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import IconButton  from "@mui/material/IconButton";
+import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
+
 import { drawerBackgroundColor } from "../utils/palette";
 
 type props = {
@@ -13,11 +16,18 @@ const SectionHeader = ({ text }: props) => {
       m={2}
       sx={{
         backgroundColor: drawerBackgroundColor,
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"space-between"
       }}
     >
       <Typography variant="h4" fontWeight={800} textTransform="uppercase">
         {text}
       </Typography>
+
+      <IconButton>
+        <AddCircleOutlinedIcon sx={{ color: "#9ccce0", fontSize: 50 }} />
+      </IconButton>
     </Box>
   );
 };
