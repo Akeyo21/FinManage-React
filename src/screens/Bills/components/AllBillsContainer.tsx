f363b8234654d8c0296461cf696b61b2e28b00fd
Akeyo21 <vickyakeke@gmail.com>
2023-05-26 21:08:43 +0300

    Add form used when adding bill

--- src/screens/Bills/components/AllBillsContainer.tsx
+++ src/screens/Bills/components/AllBillsContainer.tsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
 import BillCard from "./BillCard";
 import { billSamples } from "../Bills.sample";
-import {
-  drawerBackgroundColor,
-} from "../../../utils/palette";
+import { drawerBackgroundColor, iconsColor } from "../../../utils/palette";
 import SectionHeader from "../../../components/SectionHeader";
+import {
+  InputLabel,
+  Modal,
+  Select,
+  TextField,
+  MenuItem,
+  FormControl,
+} from "@mui/material";
+import { BillType } from "../Bills.types";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const AllBillsContainer = () => {
+  const [showAddBillModal, setShowAddBillModal] = useState<boolean>(false);
+
+  const openAddBillModal = () => {
+    setShowAddBillModal(true);
+  };
+
+  const closeAddBillModalHandler = () => {
+    setShowAddBillModal(false);
+  };
+
+  Object.values(BillType).map((billType) => console.log("TYPE", billType));
   return (
     <Grid
       item
@@ -24,13 +44,72 @@ const AllBillsContainer = () => {
         overflow: "scroll",
       }}
     >
-      <SectionHeader text="Bills" />
+      <SectionHeader text="Bills" onClickAddButtonHandler={openAddBillModal} />
 
       <Box>
         {billSamples.map((bill) => (
           <BillCard bill={bill} key={bill.id} />
         ))}
       </Box>
+
+      <Modal
+        open={showAddBillModal}
+        onClose={closeAddBillModalHandler}
+        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+      >
+        <form>
+          <Box
+            sx={{
+              width: 400,
+              backgroundColor: "white",
+              display: "flex",
+              flexDirection: "column",
+              p: 5,
+            }}
+          >
+            <Typography
+              variant="h4"
+              fontWeight={600}
+              sx={{ textTransform: "uppercase", textAlign: "center" }}
+            >
+              Add Bill
+            </Typography>
+
+            <Box sx={{ display: "flex", my: 2 }}>
+              <TextField
+                label="Bill Name"
+                id="bill-name"
+                required
+                color="primary"
+                sx={{ mr: 1 }}
+              />
+              <TextField label="Bill Amount" type="number" required />
+            </Box>
+
+            <FormControl fullWidth sx={{mb:2}}>
+              <InputLabel htmlFor="bill-due-date" />
+              <TextField id="bill-due-date" required type="date" />
+            </FormControl>
+
+            <FormControl fullWidth sx={{mb:2}}>
+              <InputLabel id="bill-type-select-label">Bill Type </InputLabel>
+              <Select label="Bill Type">
+                {Object.values(BillType).map((billType) => (
+                  <MenuItem>{billType}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <Button
+              type="submit"
+              variant="contained"
+              sx={{ color: iconsColor, ":hover":{ color:"white"} }}
+            >
+              Add Bill
+            </Button>
+          </Box>
+        </form>
+      </Modal>
     </Grid>
   );
 };
